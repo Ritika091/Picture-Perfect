23aac5fb6d01f2f9766e2b1ad8fdd11c5505182a
Ritika Saxena <ritikasaxena911@gmail.com>
2022-03-31 15:03:37 +0530

    partially responsive

--- src/components/Newpage.js
+++ src/components/Newpage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Filter from '../assets/filter.png'
 import Img1 from '../assets/image 64.png'
 import list from '../assets/list.png'
@@ -6,8 +6,9 @@ import cross from '../assets/cross.png'
 import bag from '../assets/bag.png'
 import {activity} from './data'
 import { Link } from 'react-router-dom'
+import Down from '../assets/downArrow.png'
 const Newpage = () => {
-    
+    const[more, setMore] = useState(false);
     return ( 
         <main>
     
@@ -27,14 +28,33 @@ const Newpage = () => {
              activity.map(data=>(
             <div className="activity_container">
                 <img src={data.icon} alt="" />
-                <h4>{data.type}</h4>
+                <h4 id='res_sale'>{data.type}</h4>
                 <img src={data.image} alt="" className={data.class}/>
                 <p>{data.name}</p>
               <span><img src={Img1} alt="" className='actprice' />&nbsp;&nbsp;{data.price}</span>  
               <p className='actp'>{data.from} <br /> <br />From</p>
               <p className='actp'>- <br /> <br />To</p>
               <p className='actp'>{data.time} <br /> <br />Time</p>
-             
+              {more === false?
+           <p id='mor'onClick={() => setMore(!more)} > More<img src={Down} alt="" /></p> 
+           :
+           <>
+           <p id='mor'onClick={() => setMore(!more)} > More<img src={Down} alt="" /></p> 
+           
+           <div className="res_drop" onClick={() => setMore(!more)} >
+             <div>
+         <p>0x60cC...1376 <br />
+From</p>
+<br />
+<br />
+<p>28 seconds ago
+  <br />
+Time</p>
+         </div>
+           </div>
+        
+           </>
+              }
             </div>
              ))
 }
